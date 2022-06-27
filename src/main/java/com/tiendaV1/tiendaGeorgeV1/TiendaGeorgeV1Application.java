cd90f3427bdb163bd89aa11101b5341016de3782
GeorgeX <jorgenietom83@gmail.com>
2022-06-27 14:23:17 -0500

    todo

---
+++ src/main/java/com/tiendaV1/tiendaGeorgeV1/TiendaGeorgeV1Application.java
@@ -0,0 +1,13 @@
+package com.tiendaV1.tiendaGeorgeV1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaGeorgeV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaGeorgeV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiendaV1/tiendaGeorgeV1/TiendaGeorgeV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendaV1.tiendaGeorgeV1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaGeorgeV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
