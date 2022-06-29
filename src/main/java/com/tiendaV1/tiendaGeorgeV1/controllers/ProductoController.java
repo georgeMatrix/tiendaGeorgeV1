9fa2140394866d8214a53fe0c685568dea320041
GeorgeX <jorgenietom83@gmail.com>
2022-06-29 00:28:46 -0500

    controller producto getProducto

--- src/main/java/com/tiendaV1/tiendaGeorgeV1/controllers/ProductoController.java
+++ src/main/java/com/tiendaV1/tiendaGeorgeV1/controllers/ProductoController.java
@@ -1,12 +1,21 @@
 package com.tiendaV1.tiendaGeorgeV1.controllers;
 
+import com.tiendaV1.tiendaGeorgeV1.entity.Producto;
+import com.tiendaV1.tiendaGeorgeV1.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class ProductoController {
+
+    @Autowired
+    private ProductoService productoService;
+
     @GetMapping("/producto")
-    public String getProducto(){
-     return "llegando";
+    public List<Producto> getProducto(){
+     return productoService.productos();
     }
 }
