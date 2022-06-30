367c38942b67d2744677c6558c41e2927d38bcd7
GeorgeX <jorgenietom83@gmail.com>
2022-06-29 21:55:20 -0500

    Se le puso CrossOrigin al controller

--- src/main/java/com/tiendaV1/tiendaGeorgeV1/controllers/ProductoController.java
+++ src/main/java/com/tiendaV1/tiendaGeorgeV1/controllers/ProductoController.java
@@ -3,12 +3,14 @@ package com.tiendaV1.tiendaGeorgeV1.controllers;
 import com.tiendaV1.tiendaGeorgeV1.entity.Producto;
 import com.tiendaV1.tiendaGeorgeV1.service.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProductoController {
 
     @Autowired
